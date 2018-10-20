8c4adf597a763d317118d67bdff71018fdfdb9fd
luis doriz <ldorizsalazar@gmail.com>
2018-10-19 19:09:19 -0500

    Add geolocation

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const path = require('path');
 const http = require('http');
 const express =require('express');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
 
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -23,11 +23,10 @@ io.on('connection', (socket) => {
     console.log('createdMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback('This is from the server.');
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.form,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+  });
+
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
