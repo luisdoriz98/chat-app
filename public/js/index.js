15d38a3ee16a8f184a34a76b07c61c5c0744334b
luis doriz <ldorizsalazar@gmail.com>
2018-10-21 14:24:25 -0500

    Scroll setting

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,18 @@
 var socket = io();
 
+function scrollToBottom () {
+   var messages = jQuery('#messages');
+   var newMessage = messages.children('li:last-child');
+   var clientHeight = messages.prop('clientHeight');
+   var scrollTop = messages.prop('scrollTop');
+   var scrollHeight = messages.prop('scrollHeight');
+   var newMessageHeight = newMessage.innerHeight();
+   var lastMessageHeight = newMessage.prev().innerHeight();
+
+   if (clientHeight +  scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+     messages.scrollTop(scrollHeight);
+   }
+};
 socket.on('connect', function (){
   console.log('connected to server');
 });
@@ -17,11 +30,7 @@ socket.on('newMessage', function (message) {
     createdAt:formattedTime
   });
   jQuery('#messages').append(html);
-  // var formattedTime = moment(message.createdAt).format('h:mm a');
-  // var li = jQuery('<li></li>');
-  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  //
-  // jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
